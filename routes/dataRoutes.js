0adee44cf1cfd3f884f2d4f8674cb99d03e52015
Edvinn <ealvaeus@kth.se>
2022-05-09 20:53:47 +0200

    return resolve to avoid error

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -8,31 +8,31 @@ const router = express.Router();
 router.get("/", async (req, res) => {
     try {
         const currentData = await getCurrentData();
-        res.status(200).json(currentData)
+        return res.status(200).json(currentData)
     }
     catch (err) {
-        res.status(404).json({ error: err.message })
+        return res.status(404).json({ error: err.message })
     }
 });
 
 //Endpoint for adding a reading, POST "../data"
 router.post("/", async (req, res) => {
-    if (!req.body.VOC || !req.body.CO2) {
-        res.status(409).json({ message: req.body })
+    if (!req.body.VOC || !req.body.CO2 || !req.body.time) {
+        return res.status(409).json({ error: "Need to include correct parameters." })
     }
     else if (!req.body.VOC.value || !req.body.VOC.unit) {
-        res.status(409).json({ error: "VOC parameter needs to include the fields value and unit." })
+        return res.status(409).json({ error: "VOC parameter needs to include the fields value and unit." })
     }
     else if (!req.body.CO2.value || !req.body.CO2.unit) {
-        res.status(409).json({ error: "CO2 parameter needs to include the fields value and unit." })
+        return res.status(409).json({ error: "CO2 parameter needs to include the fields value and unit." })
     }
     else {
         try {
             const newReading = await addNewData(req.body)
-            res.status(201).json(newReading)
+            return res.status(201).json(newReading)
         }
         catch (err) {
-            res.status(409).json({ error: err.message })
+            return res.status(409).json({ error: err.message })
         }
     }
 })
